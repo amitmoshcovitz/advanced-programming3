26d7cf70833238464cb33b0b6d538c4895510eea
YoavNahumus <yoavnahum2005@gmail.com>
2022-08-28 16:10:49 +0300

    fixed typo

---
+++ StandardIO.h
@@ -0,0 +1,8 @@
+#ifndef _STANDARD_IO
+#define _STANDARD_IO
+#include "DefaultIO.h"
+
+class StandardIO : protected DefaultIO {
+    
+};
+#endif
--- StandartIO.h
+++
@@ -1,8 +0,0 @@
-#ifndef _STANDART_IO
-#define _STANDART_IO
-#include "DefaultIO.h"
-
-class StandartIO : protected DefaultIO {
-    
-};
-#endif
